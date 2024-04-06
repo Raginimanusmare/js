345127efac18d20dc004fe86772768cfdcdc89a4
Raginimanusmare <142119201+Raginimanusmare@users.noreply.github.com>
2024-04-06 12:42:34 +0000

    compartionoperation

---
+++ .devcontainer/basic/compartionoperation.js
@@ -0,0 +1,20 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1);
+//console.log("02">1);
+//don't use 
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//altert
+console.log("2"===2);//check no and datatype also
+
