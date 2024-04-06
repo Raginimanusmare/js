ef805699d3956f89f3bfeb21ffbab0c586747a69
Raginimanusmare <142119201+Raginimanusmare@users.noreply.github.com>
2024-04-06 13:23:15 +0000

    Datatypesummary

---
+++ .devcontainer/basic/datatypeSummary.js
@@ -0,0 +1,37 @@
+//primative
+
+//7 type =string ,Number,Boolean,null,undefined,symbal,BigInt
+
+const score=100;
+const ScoreValue=100.3;//all number not float Decimal
+
+const isloggedIn=false;
+const outSideTemp=null;
+
+let UserEmail;
+
+const Id=Symbol('123');
+const anotherId=symbal('123');//unique output
+console.log(Id==anotherId);
+
+
+const BigNumber=12334455433n;//end n
+
+//reference or non-primative
+
+//Array,Object,Function
+const heros=["Shaktiman","doge","Spiderman"];
+
+let myObj={
+    name:"Ragini",
+    age:"22",
+
+
+}
+
+const myFunction=function() {
+
+    console.log ("Hello World");
+}
+
+console.log(ScoreValue);
