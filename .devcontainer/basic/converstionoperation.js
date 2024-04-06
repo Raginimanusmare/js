72f102261e9613275f1e43f8b274b82b4d01732e
Raginimanusmare <142119201+Raginimanusmare@users.noreply.github.com>
2024-04-06 12:18:28 +0000

    convertion2

--- .devcontainer/basic/converstionoperation.js
+++ .devcontainer/basic/converstionoperation.js
@@ -20,3 +20,43 @@ console.log(booleanInloggedIn);
 let someNumber=33
 let StringNumber=String(someNumber);
 console.log(StringNumber);
+
+//operations
+
+let value=3;
+let NegValue=-value;
+//console.log(NegValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);//2^2 power
+//console.log(2%2);
+//console.log(2/2);
+
+
+let str1="hello";
+let str2=" hi";
+let str3=str1+str2;
+console.log(str3);
+//don't write this type
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log(+true);
+//console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+let gameCounter=100;   //postfix operation
+gameCounter++;
+console.log(gameCounter);
+
+//let gameCounter=100;//prefix operation
+//++gameCounter;
+//console.log(gameCounter);
+
+
+
